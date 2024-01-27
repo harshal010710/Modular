1b5ece066df8045cc63a3e065a158c623536b523
Harshal Patil <107706576+harshal010710@users.noreply.github.com>
2024-01-27 17:50:05 +0530

    Add files via upload

---
+++ src/app/infosystems/infosystems.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InfosystemsRoutingModule } from './infosystems-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InfosystemsRoutingModule
+  ]
+})
+export class InfosystemsModule { }
